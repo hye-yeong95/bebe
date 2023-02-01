2de99ea51c7f3f149feb30ab282b4a8872020c1d
김혜영 <kimhyeyeong@gimhyeyeong-ui-MacBookAir.local>
2023-02-01 13:53:27 +0900

    Home 추가

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <div className='h-[300px] w-full flex  justify-between bg-white'>
+           <div>
+            <div>
+                <h1>Bebe Family</h1>
+                <p>다이어리</p>
+                <p>베베키트</p>
+            </div>
+            <div>
+                <div></div>
+                <p> 홈페이지 주소 </p>
+            </div>
+           </div>
+        </div>
+    );
+}
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,9 +26,9 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-            <div className='h-[700px]'>
-                <h1 className='text-center text-[60px] font-bold mt-20 mb-20'>Sevice</h1>
-                <div className='flex justify-center gap-10' >
+            <div className='h-[700px] my-32 border-b border-gray-400'>
+                <h1 className='text-center text-[60px] font-bold my-20'>Sevice</h1>
+                <div className='flex justify-center gap-10 ' >
                     <TagButton bgColor={ tagValue === 1 ? "bg-brand" : "bg-[#f95c58]/30" } text={"Bebe diary"} onClick={() => setTagValue(1)} />
                     <TagButton bgColor={ tagValue === 2 ? "bg-brand" : "bg-[#f95c58]/30" } text={"Bebe kit"} onClick={() => setTagValue(2)}/>
                     <TagButton bgColor={ tagValue === 3 ? "bg-brand" : "bg-[#f95c58]/30" } text={"Bebe mall"} onClick={() => setTagValue(3)}/>
@@ -36,6 +36,15 @@ export default function Home() {
                 <div className='m-32'>
                     <TagInfo tagValue={tagValue} />
                 </div>
+            </div>
+            <div className='my-32'>
+                <h1 className='text-center text-[60px] font-bold my-20' >아직 미정</h1>
+                    <p className='flex justify-center text-center font-semibold text-4xl'>
+                        "베베"는 우리의 서비스를 누구나 잘 이해하고<br/>
+                        가치를 공유하며, 나아가기를 희망합니다.<br/>
+                        우리가 제시하는 아이콘은 그것에 대한 첫 제시이며,<br/>
+                        더 쉽고 편한 시작이 되기를 바랍니다. 
+                    </p>
             </div>
         </div>
     );
